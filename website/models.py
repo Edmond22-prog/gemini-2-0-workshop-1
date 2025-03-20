f8db4a4cf7e7de841aac021bae5a9273e0d6a7ac
Edomondo <edghimakoll@gmail.com>
2025-03-20 01:38:43 +0100

    Adding new models for saving uploaded file with data

--- website/models.py
+++ website/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class UploadedFile(models.Model):
+    file = models.FileField(upload_to="uploads/")
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=10, null=True)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+    
+    def get_url(self):
+        return self.file.url
